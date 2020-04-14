e4ec8f0f8b2a47cf460b02b83ba4b03212117664
DrewDalmedo <andrewdalmedo@gmail.com>
2020-04-14 12:34:16 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+    if array.size == 1
+        return array[0]
+    elsif array.size == 2
+        return "#{array[0]} and #{array[1]}"
+    else
+        return_string = ""
+        index = 0
+        until index == array.size - 1
+            return_string += "#{array[index]}, "
+            index += 1
+        end
+        return_string += "and #{array.last}"
+        return return_string
+    end
 
 end
