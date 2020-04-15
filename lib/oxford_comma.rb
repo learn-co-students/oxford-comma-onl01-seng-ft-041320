7994a4142d2148f1db6405602279a621bc378fe3
Nolan Mayersky <nolyrm3@gmail.com>
2020-04-15 00:07:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  if array.length == 1 
+    return array[0]
+    elsif array.size == 2
+        return "#{array[0]} and #{array[1]}"
+      else
+        string = ""
+        count = 0 
+        while count < array.size - 1 
+          string += "#{array[count]}, "
+          count += 1
+        end
+        string += "and #{array.last}"
+        return string
+    end
 
 end
