484392caab64e015f4a03767d9fae44bc2d08ca6
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 20:05:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+if array.length == 1 #returns array to string
+    array[0]
+elsif array.length == 2
+  array.join(" and ") 
+elsif array.length >= 3
+  last_element = array.pop
+   almost_done = array.join(", ") 
+   almost_done + ", and " + last_element
+   
+   
 end
+end
+
