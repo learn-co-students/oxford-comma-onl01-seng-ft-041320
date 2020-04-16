32aae9911d7c506ea602be287310464409f3a070
Christian Cherry <christianchrr@gmail.com>
2020-04-16 19:57:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+    last_ele = array.pop
+    return array.join(", ") + ", and " + last_ele
+  end
 end
