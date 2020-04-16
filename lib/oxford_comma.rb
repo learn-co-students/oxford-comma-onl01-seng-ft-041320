fe003046965d09191effb94e14f8d135ce2e8125
Devone Austin <devone.austin@gmail.com>
2020-04-16 21:36:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.pop
+  elsif array.length == 2
+    array.join (" and ")
+  elsif array.length > 2
+    last = array.pop
+    first = array.join (", ")
+    first + (", and ") + last
+  end
 end
