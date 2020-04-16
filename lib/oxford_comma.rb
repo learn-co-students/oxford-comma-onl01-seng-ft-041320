61425f8545661c29d12b8324bb83247db5873489
Mario Garza <mario.garza02@utrgv.edu>
2020-04-16 03:50:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,6 @@ def oxford_comma(array)
   return array[0]
   elsif(array.length==2)
   return array.join(" and ")
-  #elsif(array.length==3)
-  #array[-1].insert(0,"and ")
-  #return array.join(", ")
   else
   array[-1].insert(0,"and ")
   return array.join(", ")
