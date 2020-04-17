5e4b6a6e588dc824e779613b4d62d76e984a454f
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 12:49:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+if array.size == 1
+  return array[0]
+elsif array.size == 2
+  array[1] = " and #{array[1]}"
+  return array.join
+else
+  final_element_replacement = "and #{array.last}"
+  array.pop
+  array << final_element_replacement
+  return array.join(", ")
+end
+
 
 end
