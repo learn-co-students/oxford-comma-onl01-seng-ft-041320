c43a19572e0629ae28495c263c628e18f333bff7
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-18 07:34:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
+
 def oxford_comma(array)
-   counter = 0
-  if array.length = 1
+  counter = 0
+  if array.length == 1
     array.join
-  elsif array.length = 2
+  elsif array.length == 2
     array.join(" and ")
-  elsif array.length = 3
-end
+  else 
+    array[0..-2].join(", ") + ", and "+ array[-1]
+  end
 end
