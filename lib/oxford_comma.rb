5b6375ebe35db16d059da562663562e57c604d7a
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 01:10:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+if array.length == 1 
+[array].join
+
+elsif array.length == 2
+[array].join(" and ")
+
+elsif array.length == 3
+str = ", and " + array.pop
+str = array.join(", ") << str
+
+else array.length > 3
+str = ", and " + array.pop
+str = array.join(", ") << str 
 
 end
+end
+  
