1541527b0b52e3e66fdfd6340d2b1356d7a25e87
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-21 21:34:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  counter = 0
-  if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join(" and ")
-  else 
-    array[0..-2].join(", ") + ", and "+ array[-1]
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
   end
+end
